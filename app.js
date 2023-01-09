37dfee63f5e3d4d45874d62d304fa935d19d986b
Everth Paredes <epe0413@gmail.com>
2023-01-08 20:16:58 -0600

    Construir el menú interactivo - Inquirer

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('colors');
-
-const { mostrarMenu, pausa } = require("./helpers/mensajes");
+const { inquirerMenu } = require('./helpers/inquirer');
 
 console.clear()
 
@@ -10,10 +9,8 @@ const main = async() => {
     let opt = '';
 
     do{
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({opt})
-
-        if( opt !== '0' ) await pausa();
 
     } while( opt !== '0');
     // pausa();
